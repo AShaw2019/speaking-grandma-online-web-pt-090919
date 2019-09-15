e773b5de8f078ecd59fcc024b8468ffb21933293
Ashley Twomey <aprier091012@gmail.com>
2019-09-15 17:10:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
      "I LOVE YOU TOO PUMPKIN!"
-   elsif string ==  "HUH?! SPEAK UP, SONNY!"
+   elsif string ==  
     
   elsif string == "WHAT DID YOU EAT TODAY?"
     "NO, NOT SINCE 1938!"
@@ -9,7 +9,7 @@ def speak_to_grandma(string)
      "HUH?! SPEAK UP, SONNY!"
 
   else
-    "I LOVE YOU GRANDMA!"
+   "HUH?! SPEAK UP, SONNY!"
   end
 end
 
