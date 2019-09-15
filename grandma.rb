6db49af447bd690913ed2a0213397c82a136ea3c
Ashley Twomey <aprier091012@gmail.com>
2019-09-15 16:55:58 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,20 +1,16 @@
 def speak_to_grandma(string)
-  if speak_to_grandma == "Hi Nana, how are you?"
+  if string == "Hi Nana, how are you?"
     puts "HUH?! SPEAK UP, SONNY!"
-  elsif speak_to_grandma == "WHAT DID YOU EAT TODAY?"
+  elsif string == "WHAT DID YOU EAT TODAY?"
     puts "NO, NOT SINCE 1938!"
-  elsif speak_to_grandma == "Hi!"
+  elsif string == "Hi!"
     puts "HUH?! SPEAK UP, SONNY!"
-  elsif speak_to_grandma == "I LOVE YOU GRANDMA!"
+  elsif string == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   else
     "I LOVE YOU GRANDMA!"
   end
 end
-
-x = 1
-y = 2
-x + y == 3
 
 speak_to_grandma("I LOVE YOU GRANDMA!")
 speak_to_grandma("Hi Nan, how are you?")
